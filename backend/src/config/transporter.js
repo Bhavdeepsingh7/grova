8c31936d57140c571b5f4dd41da5f5dff44b0c73
Bhavdeepsingh7 <bhavdeepmakkar@gmail.com>
2026-07-05 20:53:48 +0530

    Resolve Render SMTP ENETUNREACH by forcing IPv4 host resolution

--- backend/src/config/transporter.js
+++ backend/src/config/transporter.js
@@ -4,20 +4,24 @@ import env from "./env.js";
 
 dns.setDefaultResultOrder("ipv4first");
 
+const resolvedHost = await dns.promises
+  .lookup(env.emailHost, { family: 4, all: false })
+  .then((result) => result.address)
+  .catch(() => env.emailHost);
+
 const transportOptions = {
   auth: {
     user: env.emailUser,
     pass: env.emailPass,
   },
-  host: env.emailHost,
+  host: resolvedHost,
   port: env.emailPort,
   secure: env.emailSecure,
+  name: env.emailHost,
+  servername: env.emailHost,
   connectionTimeout: env.emailSendTimeoutMs,
   socketTimeout: env.emailSendTimeoutMs,
   family: 4,
-  lookup(hostname, options, callback) {
-    return dns.lookup(hostname, { family: 4, all: false }, callback);
-  },
 };
 
 const transporter = nodemailer.createTransport(transportOptions);
